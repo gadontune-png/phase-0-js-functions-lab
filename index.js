79448982e21722bb5f9a4cc8e3665405e337ec25
gadontune-png <gadontune@gmail.com>
2026-04-13 11:16:06 +0300

    added files

--- index.js
+++ index.js
@@ -5,3 +5,27 @@
 
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+
+ function calculateTax(amount) {
+    return amount*0.01
+ }
+
+
+ function convertToUpperCase(text){
+    return text.toUpperCase();
+ }
+
+
+ function findMaximum(num1,num2){
+    return Math.max(num1,num2);
+ }
+
+ function isPalindrome(word) {
+    const word = word.toLowerCase();
+    return word === word.split('').reverse().join('');
+
+ }
+ function calculateDiscountedPrice(originalPrice,discountPercentage) {
+    const discountAmount =(originalPrice*discountPercentage) /100;
+    return originalPrice -discountAmount;
+ }
